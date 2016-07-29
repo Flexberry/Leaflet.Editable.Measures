20bfb74a2e853d2200610c7d66dfeb060ecc317b
kaf <kaf@nevod.ru>
2016-07-29 16:58:22 +0500

    Remove unused code

--- leaflet_measure_marker.js
+++ leaflet_measure_marker.js
@@ -42,65 +42,6 @@
       return Math.abs(fixedLat).toFixed(5) + (fixedLat >= 0 ? ' с.ш. ' : ' ю.ш. ') + Math.abs(fixedLng).toFixed(5) + (fixedLng >= 0 ? ' в.д.' : ' з.д. ');
     },
 
-    /* МЕТОДЫ Leaflet.draw START */
-    /**
-     * Метод для получения нового экзепляра слоя, на основе того, который сейчас отрисовывается.
-     * @returns {Object} Новый экземпляр слоя.
-     */
-    _createLayer: function() {
-      return map.editTools.startMarker();
-    },
-
-    /**
-    * Метод для получения нового экземпляра инструмента редактирования для отрисованного слоя.
-    * @param {Object} e Аргументы метода.
-    * @param {Object} e.layer Слой, для которого требуется получить инструмент редактирования.
-    * @returns {Object} Новый экземпляр инструмента редактирования.
-    */
-    _createEditTool: function(e) {
-      ret = this.enableEdit();
-      ret.markers = [e.layer]
-      return ret;
-    },
-
-    /**
-     * Метод для привязки обработчиков событий редактирования отрисованного слоя.
-     * @param {Object} e Аргументы метода.
-     * @param {Object} e.layer Слой, редактирование которого будет обрабатываться привязываемыми обработчиками.
-     * @returns {Object} e.editTool Инструмент редактирования слоя.
-     */
-    _attachEditHandlers: function(e) {
-      var _this = this;
-      var marker = e.layer;
-      var editTool = e.editTool;
-      this._map.on ('editable:drawing:start',this._onEditToolMarkerDrag,{
-        layer: marker,
-        editTool: editTool,
-        marker: marker
-      });
-      this._map.on ('editable:dragend',this._onEditToolEditEnd,{
-        layer: marker,
-        editTool: editTool,
-        marker: marker
-      });
-
-//       marker.on('drag', function(e) {
-//         _this._onEditToolMarkerDrag.call(_this, {
-//           layer: marker,
-//           editTool: editTool,
-//           marker: marker
-//         });
-//       });
-//
-//       marker.on('dragend', function(e) {
-//         _this._onEditToolEditEnd.call(_this, {
-//           layer: marker,
-//           editTool: editTool
-//         });
-//       });
-    },
-
-/* МЕТОДЫ Leaflet.draw END */
 
       /* Методы добавленные апи переходе на Editable */
 
@@ -109,7 +50,7 @@
        */
     enable: function () {
       this.editTool = this.enableEdit();
-//       this._map.on ('editable:drawing:mouseup',function() {alert('editable:drawing:mouseup');}, this);
+      this._map.on ('editable:drawing:mouseup',function() {alert('editable:drawing:mouseup');}, this);
 // //       this._map.on ('editable:dragstart',function() {alert('editable:dragstart');}, this);
 //       this._map.on ('editable:drawing:commit',function() {alert('editable:drawing:commit');}, this);
 
